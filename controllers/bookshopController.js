2c536ce9a33846335411f3d764df17073efc0843
PietroCarlini <pietro.carlini01@gmail.com>
2026-09-19 16:16:09 +0200

    Implementing bookshop profile

--- controllers/bookshopController.js
+++ controllers/bookshopController.js
@@ -6,8 +6,20 @@ const bookshopController = {
             const bookshops = await bookshopService.getAll();
             res.status(200).json({ bookshops })
         }
-        catch(err) {
-            res.status(500).json({status:500, message: 'A server error has occured'});
+        catch (err) {
+            res.status(500).json({ status: 500, message: 'A server error has occured' });
+            console.log(err);
+        }
+    },
+
+    // GET /api/bookshops/me -> data of the logged-in bookshop (already attached to req by requireBookshop)
+    getBookshopDetails: async (req, res) => {
+        try {
+            const { id, name, city, address, email, phone, openingHours } = req.bookshop;
+            res.status(200).json({ bookshop: { id, name, city, address, email, phone, openingHours } });
+        }
+        catch (err) {
+            res.status(500).json({ status: 500, message: 'A server error has occured' });
             console.log(err);
         }
     }
--- routes/bookshopRouter.js
+++ routes/bookshopRouter.js
@@ -1,4 +1,5 @@
 const authentification = require('../middlewares/auth/authentificationMiddleware');
+const requireBookshop = require('../middlewares/auth/requireBookshopMiddleware');
 const bookshopController = require('../controllers/bookshopController');
 
 const bookshopRouter = require('express').Router();
@@ -7,6 +8,13 @@ const bookshopRouter = require('express').Router();
 bookshopRouter.get('/',
     authentification(),
     bookshopController.getAll
+);
+
+// GET /api/bookshops/me -> data of the logged-in bookshop (bookshop role only)
+bookshopRouter.get('/me',
+    authentification(),
+    requireBookshop(),
+    bookshopController.getBookshopDetails
 );
 
 module.exports = bookshopRouter;
